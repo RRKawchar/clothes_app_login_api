217f8166eb520501bd6c55afd97ce0ff4ca33b2a
Riyazur Rohman Kawchar <riyazurrohmankawchar@gmail.com>
2022-10-01 16:24:02 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/myapp/clothes_app_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.myapp.clothes_app_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:clothes_app_api/users/authentication/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Clothes app',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home:FutureBuilder(
+        builder: (context,snapshot){
+
+          return LoginScreen();
+        },
+      )
+    );
+  }
+}
+
+
